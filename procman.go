67c899b87ee0ced430cf047f0f34e173c42aa317
zj.zh <zhangzj33@gmail.com>
2017-04-19 00:51:25 +0800

    refator config file & fix bugs

--- procman.go
+++ procman.go
@@ -7,17 +7,16 @@ import (
 	"os"
 	"os/exec"
 	"sync"
+	"fmt"
+    "io"
 )
 
 type App struct {
-	Name string
-	Dir  string
+	Executable string
+	Root  string
 	Args []string
 	*os.Process
 }
-
-var managedApps = make([]*App, 5)
-var started = make(chan *App, 1)
 
 func init() {
 	handleSignals()
@@ -25,44 +24,40 @@ func init() {
 }
 
 func main() {
-	apps := make([]App, 0)
+	apps := make([]*App, 0)
 	if err := viper.UnmarshalKey("apps", &apps); err != nil {
 		panic(err)
 	}
-	log.Println(apps)
 
 	var wg sync.WaitGroup
 	for _, app := range apps {
-		log.Printf("Starting %s\n", app.Name)
+		log.Printf("Starting %s\n", app.Executable)
 		wg.Add(1)
-		go func(wg *sync.WaitGroup) {
+		go func(wg *sync.WaitGroup, app *App) {
 			defer wg.Done()
-			RunApp(&app)
-		}(&wg)
+			RunApp(app)
+		}(&wg, app)
 	}
-
-	for i := 0; i< len(apps); i++ {
-		select {
-			case a := <-started:
-				managedApps = append(managedApps, a)
-		}
-	}
-
-
-	log.Println(managedApps)
-
-	wg.Wait()
-
-	log.Println("Procman exited!")
+    
+    log.Println("Procman Started!")
+    wg.Wait()
 }
 
 func RunApp(app *App) {
-	cmdDir := app.Dir
-	cmdName := cmdDir + app.Name
-
-	cmd := exec.Command(cmdName)
-	cmd.Dir = cmdDir
-	stdout, err := cmd.StdoutPipe()
+    if len(app.Executable) == 0 {
+        panic("App Executable is empty!")
+    }
+    
+	cmd := exec.Command(app.Executable)
+    if len(app.Root) > 0 {
+        cmd.Dir = app.Root
+    }
+    
+    //var stderr io.ReadCloser
+    var stdout io.ReadCloser
+    var err error
+    //stderr, err = cmd.StderrPipe()
+    stdout, err = cmd.StdoutPipe()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -71,21 +66,27 @@ func RunApp(app *App) {
 	go func() {
 		for scanner.Scan() {
 			//TODO
-			log.Println(scanner.Text())
+			fmt.Println(scanner.Text())
+		}
+		
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "Error encountered while reading input:", err)
 		}
 	}()
 
 	if err := cmd.Start(); err != nil {
 		log.Fatal(err)
 	}
-
-
+    
+    app.Process = cmd.Process
+    log.Printf("Process %s started with PID %d\n", app.Executable, app.Process.Pid)
+    
 	if err := cmd.Wait(); err != nil {
 		if exitError, ok := err.(*exec.ExitError); ok {
 			// command fails to run or doesn't complete successfully
 			procState := exitError.ProcessState
 			if procState.Exited() {
-				log.Printf("Process %d exited with error, details: %s\n", procState.Pid(), procState.String())
+				log.Printf("Process %s(PID %d) exited with error, details: %s\n", app.Executable, procState.Pid(), procState.String())
 				//os.Exit(1)
 			} else {
 				// Not exited?
@@ -101,8 +102,7 @@ func RunApp(app *App) {
 		return 
 	}
 
-	app.Process = cmd.Process
-	started <- app
+	
 	return 
 }
 
